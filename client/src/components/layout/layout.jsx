227e6d400317f775550abb95e4aa1ebef01aa4e8
Magnus Vagn Jensen <Magnusvjensen@gmail.com>
2020-09-30 13:32:04 +0200

    started with header

--- client/src/components/layout/layout.jsx
+++ client/src/components/layout/layout.jsx
@@ -1,23 +1,39 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
+// ASSETS
+import Logo from "../../assets/images/TodHooLogo.png";
+
+// STYLE
+import "./layout.css";
+
 
 export class Layout extends Component {
     render() {
         return (
-            <div>
-                <nav>
-                    <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/about">About</Link></li>
-                    </ul>
-                </nav>
-                <main>
-                    {this.props.children}
-                </main>
-                <footer>
+            <div className="site">
+                <div className="app_continer center_middle">
+                    <header className="header">
+                        <div className="logo_container">
+                            <img className="logo" src={Logo} alt="logo" />
+                        </div>
+                        <nav className="nav">
+                            _
+                            -
+                            -
+                        {/* <ul>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/about">About</Link></li>
+                        </ul> */}
+                        </nav>
+                    </header>
+                    <main>
+                        {this.props.children}
+                    </main>
+                    {/* <footer>
                     This is a footer
-                </footer>
+                </footer> */}
+                </div>
             </div>
         )
     }

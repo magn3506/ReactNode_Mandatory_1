3266bdfaa3accc1921e6844c42c8a2da66c3e149
Magnus Vagn Jensen <Magnusvjensen@gmail.com>
2020-10-12 22:48:24 +0200

    fixed react warning.

--- client/src/pages/about/About.js
+++ client/src/pages/about/About.js
@@ -4,20 +4,20 @@ import React, { Component } from 'react'
 import "./about.css";
 
 // LAYOUT
-import LAYOUT, { Layout } from "../../components/layout/layout"
+import LAYOUT from "../../components/layout/layout"
 
 export class About extends Component {
     render() {
         return (
-            <Layout nav logo>
+            <LAYOUT nav logo>
                 <div className="about_content">
                     <h1>ABOUT</h1>
                     <p>This app is created a assignment for KEA Web-development React/node.js elective</p>
                     <p>The app is a fullstack solution using, REACT AND EXPRESS. The data is saved in a simple json file.</p>
-                    <p>Checkout the source code <a target="_blank" href="https://github.com/magn3506/ReactNode_Mandatory_1">here</a></p>
+                    <p>Checkout the source code <a target="_blank" href="https://github.com/magn3506/ReactNode_Mandatory_1" rel="noopener noreferrer">here</a></p>
                 </div>
 
-            </Layout>
+            </LAYOUT>
         )
     }
 }

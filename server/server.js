f422b1c185016f22550ea3d06732722b72335409
Magnus Vagn Jensen <Magnusvjensen@gmail.com>
2020-10-13 09:44:40 +0200

    changed server

--- server/server.js
+++ server/server.js
@@ -30,10 +30,10 @@ app.use(express.json()); // ??
 app.use("/api/todo", todo);
 
 // ROOT INDEX
-app.get("/", (req, res) => {
-    // handle root
-    res.send("Hello");
-})
+// app.get("/", (req, res) => {
+//     // handle root
+//     res.send("Hello");
+// })
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.

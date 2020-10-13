862e9a1b29805eda7e7e172f08877aa8f7fd862c
Magnus Vagn Jensen <Magnusvjensen@gmail.com>
2020-10-13 09:27:57 +0200

    ok

--- server/server.js
+++ server/server.js
@@ -35,6 +35,11 @@ app.get("/", (req, res) => {
     res.send("Hello");
 })
 
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 // LISTEN
 app.listen(PORT, err => {

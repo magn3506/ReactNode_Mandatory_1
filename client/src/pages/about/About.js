4f0463696b05c19bb9aa63bf23f0b04f2bcc1c11
Magnus Vagn Jensen <Magnusvjensen@gmail.com>
2020-10-14 14:46:45 +0200

    corrected spelling in about page

--- client/src/pages/about/About.js
+++ client/src/pages/about/About.js
@@ -12,11 +12,10 @@ export class About extends Component {
             <LAYOUT nav logo>
                 <div className="about_content">
                     <h1>ABOUT</h1>
-                    <p>This app is created a assignment for KEA Web-development React/node.js elective</p>
-                    <p>The app is a fullstack solution using, REACT AND EXPRESS. The data is saved in a simple json file.</p>
+                    <p>This app is created as an assignment for KEA Web-development React/node.js elective</p>
+                    <p>The app is a fullstack solution using, REACT and EXPRESS. The data is saved in a simple json file.</p>
                     <p>Checkout the source code <a target="_blank" href="https://github.com/magn3506/ReactNode_Mandatory_1" rel="noopener noreferrer">here</a></p>
                 </div>
-
             </LAYOUT>
         )
     }
